614a1a6373c5628eb01585268ed4e530c6216ab4
Aditya Singh <122855095+adityasingh151@users.noreply.github.com>
2024-11-21 00:18:54 +0530

    frontend url update in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
+    allow_origins=["https://diet-frontend.onrender.com/"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
