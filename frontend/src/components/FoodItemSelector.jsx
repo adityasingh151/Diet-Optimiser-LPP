cb78cf368e2d6ebb165cc3eac4d53828bb548169
Aditya Singh <122855095+adityasingh151@users.noreply.github.com>
2024-11-30 15:54:44 +0530

    Update FoodItemSelector.jsx

--- frontend/src/components/FoodItemSelector.jsx
+++ frontend/src/components/FoodItemSelector.jsx
@@ -12,7 +12,7 @@ const FoodItemSelector = () => {
   // Fetch food items on component mount
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/menu")
+      .get("https://diet-backend-d1gj.onrender.com/menu")
       .then((response) => setFoodItems(response.data))
       .catch((error) => {
         console.error("Error fetching food items:", error);
